428c03763cd765131a5cdb896c2f0d95a6b409e7
kehinde obanla <kehinde.o.obanla@gmail.com>
2021-02-03 22:18:51 -0600

    added getch

--- shell2.py
+++ shell2.py
@@ -1,15 +1,11 @@
 import threading
-
 import os
-
+import sys
 from numpy.core.records import array
-
 import cmd_pkg as cp
-
 from cmd_pkg import parameters as par
-
-
 import numpy as np
+import getch as gt
 
 
 class CommandHelper(object):
@@ -17,55 +13,33 @@ class CommandHelper(object):
     def __init__(self):
 
         self.commands = {}
-
         self.commands['ls'] = cp.ls
-
         self.commands['cd'] = cp.cd
-
         self.commands['pwd'] = cp.pwd
-
         self.commands['mkdir'] = cp .mkdir
-
         self.commands['cp'] = cp. cp
-
         self.commands['rm'] = cp. rm
-
         self.commands['cat'] = cp.cat
-
         self.commands['mv'] = cp.mv
-
         self.commands['rmdir'] = cp.rmdir
-
         self.commands['exit'] = cp.exit
-
         self.commands['wc'] = cp.wc
-
         self.commands['grep'] = cp.grep
         self.commands['history'] = cp.history
         self.commands['findhistory'] = cp.findhistory
 
         self.possibleflag = par.flag()
-
         self.possibleDirections = par.direct()
-
         self.possibleparameters = par.possibleparameters()
-
         self.pipe = par.piped()
 
     def parseArgs(self, **kwargs):
-
         flags = []
-
         params = []
-
         directions = []
-
         cmd = kwargs['cmd']
-
         args = kwargs['params']
-
         tag = kwargs['tag']
-
         indexArray = []
 
         if(len(args) > 0):
@@ -123,82 +97,54 @@ class CommandHelper(object):
             return self.invoke(cmd=cmd, flags=flags, params=params, directions=directions)
 
     def invoke(self, **kwargs):
-
         if 'cmd' in kwargs:
-
             cmd = kwargs['cmd']
-
         else:
-
             cmd = ''
-
         if 'params' in kwargs:
-
             params = kwargs['params']
-
         else:
-
             params = []
-
         if 'flags' in kwargs:
-
             flags = kwargs['flags']
-
         else:
-
             flags = []
-
         if 'directions' in kwargs:
-
             directions = kwargs['directions']
-
         else:
-
             directions = []
-
         if 'tag' in kwargs:
-
             tag = kwargs['tag']
-
         else:
-
             tag = False
-
         if 'thread' in kwargs:
-
             thread = kwargs['thread']
-
         else:
-
             thread = False
-
         # One way to invoke using dictionary
 
         if not thread:
-
             return self.commands[cmd](flags=flags, params=params, directions=directions, tag=tag)
-
         else:
-
-            # Using a thread ****** broken right now *********
-
             if len(params) > 0:
-
                 c = threading.Thread(
                     target=self.commands[cmd], args=tuple(kwargs))
-
             else:
-
                 c = threading.Thread(target=self.commands[cmd])
-
             c.start()
-
             c.join()
 
     def exists(self, cmd):
-
         return cmd in self.commands
-
+        
+    def print_cmd(cmd,prompt):
+        """ This function "cleans" off the command line, then prints
+            whatever cmd that is passed to it to the bottom of the terminal.
+        """
+        padding = " " * 80
+        sys.stdout.write("\r"+padding)
+        sys.stdout.write("\r"+prompt+cmd)
+        sys.stdout.flush()
 
 if __name__ == '__main__':
 
@@ -231,67 +177,34 @@ if __name__ == '__main__':
         answer = ""
 
         if('|' in command_input):
-
             command_input = command_input.split('|')
-
             length = len(command_input)
-
             for i in range(0, len(command_input)):
-
                 split2 = command_input[i].split()
-
                 cmd = split2[0]
-
                 params = split2[1:]
-
                 if(i != length - 1):
-
                     if ch.exists(cmd):
-
                         answer = ch.parseArgs(cmd=cmd, params=params, tag=tag)
-
                     else:
-
                         answer = "Error: command %s doesn't exist." % (cmd)
-
                         break
-
                 else:
-
                     if ch.exists(cmd):
                         tag = True
                         params.append(answer)
-
                         answer = ch.parseArgs(cmd=cmd, params=params, tag=tag)
-
                     else:
-
                         answer = "Error: command %s doesn't exist." % (cmd)
-
                         break
-
             print(answer)
 
         else:
-
             command_input = command_input.split()
-
-            # params are all but first position in list
-
             params = command_input[1:]
-
-            # pull actual command from list
-
             cmd = command_input[0]
-
-            # if command exists in our shell
-
             if ch.exists(cmd):
-
                 answer = ch.parseArgs(cmd=cmd, params=params, tag=tag)
-
             else:
-
                 answer = "Error: command %s doesn't exist." % (cmd)
-
             print(answer)
