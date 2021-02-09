710f7d852140180b791fd67253e7c075ce1d5dd5
kehinde obanla <kehinde.o.obanla@gmail.com>
2021-02-08 19:04:32 -0600

    updated cat and mv

--- cmd_pkg/cat.py
+++ cmd_pkg/cat.py
@@ -4,19 +4,55 @@ import sys
 import glob
 import shutil
 from pathlib import Path
- #If the parameters are just files, print them on the screen
+# If the parameters are just files, print them on the screen
+
+
 def cat(**kwargs):
-	command = ['cat']
-	params = kwargs['params']
-	flag = kwargs['flags'] 
-	directions = kwargs['directions']  
-	tag = kwargs['tag']
-	length = len(params)
-	filecount = 0
-	for p in params:
-		with open(params[filecount], "r") as f:
-			lines = f.read().splitlines()
-	for line in lines:
-		print(line)
-	print(" ")
-	filecount+=1
+    command = ['cat']
+    parameter = kwargs['params']
+    flag = kwargs['flags']
+    directions = kwargs['directions']
+    tag = kwargs['tag']
+    length = len(parameter)
+    answer = ''
+    if(len(flag) == 0 and length > 0):
+
+        if(tag == False):
+            for p in parameter:
+                lines = []
+                with open(p, "r") as f:
+                    lines = f.read().splitlines()
+                for line in lines:
+                    answer = answer + line
+                    answer = answer + '\n'
+            if(len(directions) == 2):
+                direct = directions[0]
+                fil = directions[1]
+                with open(fil, direct) as f:
+                    f.write(answer)
+            else:
+                return answer
+        else:
+            files = parameter[0].split("\n")
+            for fil in files:
+                lines =[]
+                if(os.path.isfile(fil)):
+                    with open(fil,'r') as f:
+                        lines = f.read().splitlines()
+                    for line in lines:
+                        answer = answer + line
+                        answer = answer +'\n'
+                    if(len(directions) == 2):
+                        direct = directions[0]
+                        fil = directions[1]
+                        with open(fil, direct) as fi:
+                            fi.write(answer)
+                    
+                else:
+                    answer = answer+ "{} is not a file".format(fil)
+            return answer
+
+    else:
+        answer = "invalid parameters"
+			
+
--- cmd_pkg/mv.py
+++ cmd_pkg/mv.py
@@ -1,30 +1,64 @@
 import os
-import threading
-import sys
-import glob
+
+from os.path import isfile
+
 import shutil
+
 from pathlib import Path
 
 
+# function to move a file
 
-#function to move a file
 def mv(**kwargs):
-	command = ['mv']
-	parameter = kwargs['params']
-	
-	flag = kwargs['flags'] 
-	directions = kwargs['directions']  
-	tag = kwargs['tag']
-	length = len(parameter)
-	path = os.getcwd()
-	#if there are not enough parameters
-	if(len(parameter) <2 and len(flag) == 0 and len(directions)== 0 and tag ==False):
-		print("not enough arguments given")
-	else:
-		file1 = parameter[0]
-		for x in range(length-1):
-			toMove = parameter[x]
-			source = path+"/"+toMove			
-			destination = path+"/"+parameter[length-1]
-			if(os.path.exists(source) & os.path.exists(destination)):
-				shutil.move(source,destination)
+
+    command = ['mv']
+
+    parameter = kwargs['params']
+
+    flag = kwargs['flags']
+
+    directions = kwargs['directions']
+
+    tag = kwargs['tag']
+
+    length = len(parameter)
+    lenm = int(length - 1)
+
+    path = os.getcwd()
+
+    answer = ""
+
+    # if there are not enough parameters
+
+    if(len(parameter) < 2 and len(flag) == 0 and len(directions) == 0 and tag == False):
+
+        answer = "not enough arguments given"
+
+        return answer
+
+    elif(len(parameter) >= 2 and len(flag) == 0 and len(directions) == 0 and tag == False):
+
+        para = parameter[:lenm]
+
+        destination = parameter[-1]
+
+        print(destination)
+
+        for source in para:
+
+            if(os.path.exists(source) & os.path.exists(destination)):
+
+                shutil.move(source, destination)
+
+            else:
+
+                answer = answer + \
+                    "{} or {} does not exist".format(source, destination)
+
+                return answer
+
+    else:
+
+        answer = "invalid argument"
+
+        return answer
